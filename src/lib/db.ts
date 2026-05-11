6c72f8283d4e4e13053fb5131cb34008fafcb184
Margaryta Viter <margarytaviter@gmail.com>
2026-05-11 12:56:52 +0300

    adjust deploying db process

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,8 +3,8 @@ import { Pool as PgPool } from "pg";
 
 const isNeon = process.env.DATABASE_URL?.includes("neon.tech");
 
-const pool = isNeon
-  ? new NeonPool({ connectionString: process.env.DATABASE_URL })
+const pool: PgPool = isNeon
+  ? (new NeonPool({ connectionString: process.env.DATABASE_URL }) as unknown as PgPool)
   : new PgPool({ connectionString: process.env.DATABASE_URL });
 
 export default pool;
